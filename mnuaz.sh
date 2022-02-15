7d42bab1406c8d52b31fe856d5d4bf172bafb5c9
Diego E. KESSELMAN <42894928+dkesselman@users.noreply.github.com>
2022-02-15 09:34:44 -0600

    Changes for using cdialog

--- mnuaz.sh
+++ mnuaz.sh
@@ -3,20 +3,36 @@
 QIBM_MULTI_THREADED='Y'
 export QIBM_MULTI_THREADED
 
-
 source mnuaz_const.sh
 source mnuaz_inc.sh
 
 #***********************************************************************************#
 
-  while [[ "$O" != " " ]]; do case $i in
-        0) S=M0;SC;if [[ $cur == "" ]];then R;$e "\n$( BUCKETLS  )\n";ES;fi;;
-        1) S=M1;SC;if [[ $cur == "" ]];then R;$e "\n$( BUCKETPT  )\n";ES;fi;;
-        2) S=M2;SC;if [[ $cur == "" ]];then R;$e "\n$( BUCKETGT  )\n";ES;fi;;
-        3) S=M3;SC;if [[ $cur == "" ]];then R;$e "\n$( BUCKETDL  )\n";ES;fi;;
-        4) S=M4;SC;if [[ $cur == "" ]];then R;$e "\n$( BKPTOCLD  )\n";ES;fi;;
-        5) S=M5;SC;if [[ $cur == "" ]];then R;$e "\n$( BKPTOCLD2 )\n";ES;fi;;  
-        6) S=M6;SC;if [[ $cur == "" ]];then R;$e "\n$( LSTCLDBKP )\n";ES;fi;;  
-        7) S=M7;SC;if [[ $cur == "" ]];then R;$e "\n$($e IBMi to AZURE Menu by ESSELWARE  )\n";ES;fi;;
-        8) S=M8;SC;if [[ $cur == "" ]];then R;exit 0;fi;;
- esac;POS;done
+EXIT="NO";
+
+while [[ "$EXIT" == "NO" ]]; 
+	resp=$(dialog --clear --title "BACKUP IBM i to AZURE - MENU" \
+		--ok-label "Select" --cancel-label "Exit" --stdout \
+        --menu "Select your option:" 20 51 9  \
+        0  "List content on AZURE         " \
+        1  "Upload file to  AZURE         " \
+        2  "Get file from AZURE           " \
+        3  "Delete file from AZURE        " \
+        4  "Save Library to AZURE         " \
+        5  "Save ALL Libraries to AZURE   " \
+		6  "List library saved on AZURE   " \
+		7  "ABOUT                         " )
+## dialog --title "Selection" --msgbox $resp 0 0;
+	do case $resp in
+        0) BUCKETLS ;;
+        1) BUCKETPT ;;
+        2) BUCKETGT ;;
+        3) BUCKETDL ;;
+        4) BKPTOCLD ;;
+        5) BKPTOCLD2;;
+        6) LSTCLDBKP;;
+        7) dialog --title "ABOUT THIS SOFTWARE" --msgbox "\n       IBM i Backup to AZURE BLOB Menu\n\n\n         by ESSELWARE SOLUCIONES" 10 50 ;;
+	esac;
+done
+clear
+exit 0
